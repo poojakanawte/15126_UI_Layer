1e3911fa5e1d4a14df01d66e688eb3141367c148
poojakanawte <63844297+poojakanawte@users.noreply.github.com>
2021-10-21 22:01:33 +0530

    Add files via upload

---
+++ AssignmentOOPS/HRLib/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLib
+{
+    class Employee
+    {
+        private string empName;
+
+        public string EmpName
+        {
+            get { return  empName; }
+            set { empName = value; }
+
+        }
+
+        public string address;
+
+        public string Address
+        {
+            get { return address; }
+            protected set { address = value; }
+
+        }
+        public Employee()
+        {
+            EmpName = "pooja" ;
+            Address = "mahape";
+        }
+        public Employee(string name, string address)
+        {
+            this.EmpName = name;
+            this.Address = address;
+        }
+
+    }
+}
