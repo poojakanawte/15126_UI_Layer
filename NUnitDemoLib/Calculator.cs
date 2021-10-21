6bb1654a05b94986677b10cce717432a59fe85e6
poojakanawte <63844297+poojakanawte@users.noreply.github.com>
2021-10-21 22:34:38 +0530

    Add files via upload

---
+++ NUnitDemoLib/Calculator.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+namespace NUnitDemoLib
+{
+    public class Calculator
+    {
+        public int Adition(int first, int second)
+        {
+            return first + second;
+        }
+        public int Subtraction(int first, int second)
+        {
+            if (first < second)
+            {
+                throw new ArgumentException($"First number {first} is less than Second number {second}");
+            }
+            return first - second;
+        }
+    }
+}
+
