9db93a2b92330fd78df4bd190fe065ecd89454e9
poojakanawte <63844297+poojakanawte@users.noreply.github.com>
2021-10-21 22:14:44 +0530

    Add files via upload

---
+++ EFAssignment/EFAssignment/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace EFAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnSignin = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(104, 55);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(94, 25);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "UserName";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(104, 160);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(87, 25);
+            this.Password.TabIndex = 1;
+            this.Password.Text = "Password";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(265, 52);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(150, 31);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(265, 160);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(150, 31);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnSignin
+            // 
+            this.btnSignin.Location = new System.Drawing.Point(265, 290);
+            this.btnSignin.Name = "btnSignin";
+            this.btnSignin.Size = new System.Drawing.Size(112, 34);
+            this.btnSignin.TabIndex = 4;
+            this.btnSignin.Text = "Signin";
+            this.btnSignin.UseVisualStyleBackColor = true;
+            this.btnSignin.Click += new System.EventHandler(this.btnSignin_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(117, 255);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(47, 25);
+            this.lblMsg.TabIndex = 5;
+            this.lblMsg.Text = "Msg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnSignin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.lblUserName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox txtUSerName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnSignin;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.TextBox txtUserName;
+    }
+}
+
---
+++ EFAssignment/EFAssignment/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFAssignment.Models;
+using System.Windows.Forms;
+
+namespace EFAssignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignin_Click(object sender, EventArgs e)
+        {
+            TrainingContext db1 = new TrainingContext();
+
+
+
+            var q = from p in db1.Userdata
+
+
+
+                    where p.Username == txtUserName.Text
+
+
+
+                    && p.Password == txtPassword.Text
+
+
+
+                    select p;
+
+
+
+            if (q.Any())
+
+
+
+            {
+
+
+
+                Console.WriteLine(lblMsg.Text = "Valid User");
+
+
+
+            }
+
+
+
+            else
+
+
+
+            {
+
+
+
+                Console.WriteLine(lblMsg.Text = "InValid User");
+
+
+
+            }
+
+
+
+
+        }
+        }
+}
