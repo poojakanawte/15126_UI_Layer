387ae2b2cc3400515375b7328eb5baa0d5152432
poojakanawte <63844297+poojakanawte@users.noreply.github.com>
2021-10-21 21:56:35 +0530

    Add files via upload

---
+++ DelegateDemoMastek/DelegateDemoMastek/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DelegateDemoMastek
+{
+    delegate int CalculatorDelegate(int first, int second);
+    class Program
+    {
+
+       /* static int MulNumbers(int number1,int number2)
+        {
+            return number1 * number2;
+        }
+        static int AddNumbers(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+       */
+
+        static void Main(string[] args)
+        {
+
+
+            int result;
+
+            //int result1 = demo2(5, 6);
+           
+           
+            Console.WriteLine("-----------");
+
+            CalculatorDelegate demo2 = (a, b) => a + b;
+            result = demo2(12, 5);
+            Console.WriteLine("addition" + result);
+            
+
+            CalculatorDelegate demo3 = (a, b) => a - b;
+            result = demo3(12, 5);
+            Console.WriteLine("Substraction" + result);
+          
+
+            CalculatorDelegate demo4 = (a, b) => a % b;
+            result = demo4(12, 5);
+            Console.WriteLine("Division" + result);
+
+
+
+            CalculatorDelegate demo5 = (a, b) => a * b;
+            result = demo5(12, 5);
+            Console.WriteLine("Multiplication" + result);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
